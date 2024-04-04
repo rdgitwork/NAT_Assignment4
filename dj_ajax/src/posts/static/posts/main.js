5ac3554f227fbf130b088183cf0f44d0c13465f0
Rudrakumar Patel <rdgitwork@gmail.com>
2024-04-04 14:30:35 -0400

    Make a post card and add buttons.

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -2,7 +2,7 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 const postsBox = document.getElementById('posts-box')
-
+const spinnerBox = document.getElementById('spinner-box')
 
 
 $.ajax({
@@ -23,13 +23,30 @@ $.ajax({
     success: function(response) {
         console.log(response)
         const data = response.data
-        console.log(data)
-        data.forEach(el => {
-            postsBox.innerHTML += `
-            ${el.title} - <b>${el.body}</b><br>
-            `
-        });
-
+        setTimeout(() => {
+            spinnerBox.classList.add('not-visible')
+            console.log(data)
+            data.forEach(el => {
+                postsBox.innerHTML += `
+                <div class="card mb-2">
+                <div class="card-body">
+                <h5 class="card-title">${el.title}</h5>
+                <p class="card-text">${el.body}</p>
+                </div>
+                <div class="card-footer">
+                <div class="row">
+                <div class="col-2">
+                <a href="#" class="btn btn-primary">Details</a>
+                </div>
+                <div class="col-1">
+                <a href="#" class="btn btn-primary">Like</a>
+                </div>
+                </div>
+                </div>
+                </div>
+                `
+            });
+        }, 100)
     },
     error: function(error) {
         console.log('error', error)
