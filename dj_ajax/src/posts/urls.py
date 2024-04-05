99278022569fc6a1f710b3eb14227653489813f4
Rudrakumar Patel <rdgitwork@gmail.com>
2024-04-05 05:21:49 -0400

    Setup the details button in the posts.

--- dj_ajax/src/posts/urls.py
+++ dj_ajax/src/posts/urls.py
@@ -3,7 +3,7 @@ from .views import (
     post_list_and_create,
     load_post_data_view,
     like_unlike_post,
-    hello_world_view,
+    post_detail,
 )
 
 app_name = 'posts'
@@ -13,7 +13,8 @@ urlpatterns = [
     path('', post_list_and_create, name='main-board'),
     path('like-unlike/', like_unlike_post, name='like-unlike'),
 
-    path('data/<int:num_posts>/', load_post_data_view, name='posts-data'),
+    path('<pk>/', post_detail, name='post-detail'),
 
-    path('hello-world/', hello_world_view, name='hello-world'),
+    path('data/<int:num_posts>/', load_post_data_view, name='posts-data')
+
 ]
