1d9c9b24ac508b801bb384d7b27f133b1a775c3d
Rudrakumar Patel <rdgitwork@gmail.com>
2024-04-05 05:06:51 -0400

    Clear form after adding a new post.

--- dj_ajax/src/posts/static/posts/main.js
+++ dj_ajax/src/posts/static/posts/main.js
@@ -154,6 +154,7 @@ postForm.addEventListener('submit', e=> {
             likeUnlikePosts()
             $('#addPostModal').modal('hide')
             handleAlerts('success', 'New post added!')
+            postForm.reset()
         },
         error: function(error){
             console.log(error)
