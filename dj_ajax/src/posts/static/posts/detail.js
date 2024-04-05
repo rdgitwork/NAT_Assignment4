59ae190b533e8ab03510625fc69d12390989a7a9
Rudrakumar Patel <rdgitwork@gmail.com>
2024-04-05 19:13:02 -0400

    Add alert box for the update and delete button.

--- dj_ajax/src/posts/static/posts/detail.js
+++ dj_ajax/src/posts/static/posts/detail.js
@@ -1,10 +1,13 @@
 console.log('hello world')
 
+const postBox = document.getElementById('post-box')
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById('spinner-box')
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
 
 backBtn.addEventListener('click', () => {
     history.back()
@@ -27,6 +30,20 @@ $.ajax({
 
         }
 
+        const titileEl = document.createElement('h3')
+        titileEl.setAttribute('class', 'mt-3')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titileEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titileEl)
+        postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
 
         spinnerBox.classList.add('not-visible')
     },
